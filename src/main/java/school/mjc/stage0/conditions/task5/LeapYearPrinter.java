5965b1ef2e0bfa6ccb7cfbd37aeb1d641db27ad3
Nicholas <urkona@list.ru>
2023-11-12 14:17:01 +0300

    init: init task

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        System.out.println(_isLeapYear(year) ? "leap" : "not leap");
     }
+
+    private boolean _isLeapYear(int year) {
+        if (year % 4 != 0) {
+            return false;
+        }
+        if (year % 100 != 0) {
+            return true;
+        }
+        if (year % 400 != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
